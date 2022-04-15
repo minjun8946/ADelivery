d6558c0354c6eb39e1dcb996ab787c34f3fdb5f0
minjun8946 <minjun8946@gmail.com>
2022-04-15 12:33:16 +0900

    ⚡️:: data gradle

---
+++ data/build.gradle.kts
@@ -0,0 +1,46 @@
+import com.adelivery.buildsrc.Libraries
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles ("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(Libraries.Activity.core)
+    implementation(Libraries.Activity.appCompat)
+    implementation(Libraries.Coroutines.coroutines)
+    implementation(Libraries.Hilt.inject)
+    implementation(Libraries.Room.room)
+    implementation(Libraries.Room.roomKtx)
+    implementation(Libraries.Room.roomCompiler)
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+}
