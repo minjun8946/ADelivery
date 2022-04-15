74ff12efd90081cdcd1b8ee8964d233f5105f386
minjun8946 <minjun8946@gmail.com>
2022-04-15 11:56:30 +0900

    ⚡️:: versions

---
+++ buildSrc/src/main/java/com/adelivery/buildsrc/Versions.kt
@@ -0,0 +1,29 @@
+package com.adelivery.buildsrc
+
+object Versions {
+
+    /* build tools */
+    const val buildTools = "31.0.0"
+
+    /* plugins */
+    const val androidPlugin = "7.1.0"
+    const val googleServices = "4.3.10"
+
+    /* libraries */
+    const val compose = "1.0.1"
+    const val composeConstraint = "1.0.0"
+    const val navigation = "2.4.0-rc01"
+    const val core = "1.7.0"
+    const val activity = "1.3.1"
+    const val appCompat = "1.3.1"
+    const val lifecycle = "2.4.0"
+    const val hilt = "2.38.1"
+    const val hiltCompose = "1.0.0-rc01"
+    const val coroutines = "1.6.0"
+    const val retrofit = "2.9.0"
+    const val okhttp = "3.14.9"
+    const val test = "4.13.1"
+    const val room = "2.4.0"
+    const val moshi = "1.12.0"
+    const val moshiKotlin = "1.8.0"
+}
