6dd16e2c236f0a1a336a5d9f7763c6859bf80c46
minjun8946 <minjun8946@gmail.com>
2022-04-19 22:35:21 +0900

    ⚡️:: update delivery dao

--- data/src/main/java/com/adelivery/data/local/dao/DeliveryDao.kt
+++ data/src/main/java/com/adelivery/data/local/dao/DeliveryDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.adelivery.data.local.entity.DeliveryBasicRoomEntity
 import com.adelivery.data.local.entity.DeliveryCheckRoomEntity
 import com.adelivery.data.local.entity.DeliveryCompanyRoomEntity
 
@@ -16,9 +17,16 @@ interface DeliveryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDeliveryCheck(deliveryCheckRoomEntity: DeliveryCheckRoomEntity)
 
-    @Query("SELECT * FROM deliverycheckroomentity")
-    fun fetchDeliveryCheck(): DeliveryCheckRoomEntity
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDeliveryBasicRoom(deliveryBasicRoomEntity: DeliveryBasicRoomEntity)
+
+    @Query("SELECT * FROM deliverycheckroomentity WHERE trackId== :trackId")
+    fun fetchDeliveryCheck(trackId: Int): DeliveryCheckRoomEntity
 
     @Query("SELECT * FROM deliverycompanyroomentity")
-    fun fetchDeliveryCompany(): DeliveryCompanyRoomEntity
+    fun fetchDeliveryCompany(): List<DeliveryCompanyRoomEntity>
+
+    @Query("SELECT *FROM deliverybasicroomentity")
+    fun fetchDeliveryBasic(): List<DeliveryBasicRoomEntity>
+
 }
