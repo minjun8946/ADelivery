ba27a7d3afee10cfdc54ebc89cd2e1891238e7d8
minjun8946 <minjun8946@gmail.com>
2022-04-19 22:39:22 +0900

    ⚡️:: toentity

---
+++ data/src/main/java/com/adelivery/data/remote/response/DeliveryCompanyResponse.kt
@@ -0,0 +1,18 @@
+package com.adelivery.data.remote.response
+
+import com.adelivery.domain.entity.DeliveryCompanyEntity
+import com.google.gson.annotations.SerializedName
+
+data class DeliveryCompanyResponse(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("tel") val tel: String
+)
+
+fun DeliveryCompanyResponse.toEntity() =
+    DeliveryCompanyEntity(
+        id = id,
+        name = name,
+        tel = tel
+    )
+
