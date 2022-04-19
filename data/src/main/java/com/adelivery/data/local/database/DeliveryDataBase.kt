1f3fa91341fd6ab4ee47f5e6a693b10db76931d4
minjun8946 <minjun8946@gmail.com>
2022-04-19 22:35:42 +0900

    ⚡️:: add entities

--- data/src/main/java/com/adelivery/data/local/database/DeliveryDataBase.kt
+++ data/src/main/java/com/adelivery/data/local/database/DeliveryDataBase.kt
@@ -3,22 +3,22 @@ package com.adelivery.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.adelivery.data.local.converter.DeliveryCompanyConverter
 import com.adelivery.data.local.converter.DeliveryProgressConverter
 import com.adelivery.data.local.dao.*
+import com.adelivery.data.local.entity.DeliveryBasicRoomEntity
 import com.adelivery.data.local.entity.DeliveryCheckRoomEntity
 import com.adelivery.data.local.entity.DeliveryCompanyRoomEntity
 
 @Database(
     entities = [
         DeliveryCheckRoomEntity::class,
-        DeliveryCompanyRoomEntity::class
+        DeliveryCompanyRoomEntity::class,
+        DeliveryBasicRoomEntity::class
     ], version = 1, exportSchema = false
 )
 
 @TypeConverters(
     value = [
-        DeliveryCompanyConverter::class,
         DeliveryProgressConverter::class
     ]
 )
