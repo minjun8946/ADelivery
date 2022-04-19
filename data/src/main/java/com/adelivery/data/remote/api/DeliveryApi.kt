a0ee2af49f8680fe0d511476fc5ef062774d2d2b
minjun8946 <minjun8946@gmail.com>
2022-04-19 22:37:39 +0900

    🐛:: change list type

--- data/src/main/java/com/adelivery/data/remote/api/DeliveryApi.kt
+++ data/src/main/java/com/adelivery/data/remote/api/DeliveryApi.kt
@@ -1,17 +1,17 @@
 package com.adelivery.data.remote.api
 
-import com.adelivery.data.remote.response.FetchDeliveryCompanyResponse
-import com.adelivery.data.remote.response.PatchDeliveryCheckResponse
+import com.adelivery.data.remote.response.DeliveryCompanyResponse
+import com.adelivery.data.remote.response.DeliveryCheckResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface DeliveryApi{
     @GET("/")
-    suspend fun fetchDeliveryCompany(): FetchDeliveryCompanyResponse
+    suspend fun fetchDeliveryCompany(): List<DeliveryCompanyResponse>
 
     @GET("{carrier_id}/tracks/{track_id}")
     suspend fun patchDeliveryCheck(
         @Path("carrier_id") carrierId: String,
         @Path("track_id") trackId: Int
-    ): PatchDeliveryCheckResponse
+    ): DeliveryCheckResponse
 }
