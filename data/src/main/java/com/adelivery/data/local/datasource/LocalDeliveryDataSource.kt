df472bd18a6b5451b7123203ef5736080fc37100
minjun8946 <minjun8946@gmail.com>
2022-04-19 22:35:59 +0900

    🐛:: delivery data source

--- data/src/main/java/com/adelivery/data/local/datasource/LocalDeliveryDataSource.kt
+++ data/src/main/java/com/adelivery/data/local/datasource/LocalDeliveryDataSource.kt
@@ -1,14 +1,23 @@
 package com.adelivery.data.local.datasource
 
+import com.adelivery.data.local.entity.DeliveryBasicRoomEntity
 import com.adelivery.data.local.entity.DeliveryCheckRoomEntity
 import com.adelivery.data.local.entity.DeliveryCompanyRoomEntity
+import com.adelivery.data.remote.request.DeliveryCheckRequest
+import com.adelivery.domain.entity.DeliveryBasicEntity
+import com.adelivery.domain.entity.DeliveryCheckEntity
+import com.adelivery.domain.entity.DeliveryCompanyEntity
 
 interface LocalDeliveryDataSource {
     suspend fun insertDeliveryCheck(deliveryCheckRoomEntity: DeliveryCheckRoomEntity)
 
     suspend fun insertDeliveryCompany(deliveryCompanyRoomEntity: DeliveryCompanyRoomEntity)
 
-    suspend fun fetchDeliveryCheck(): DeliveryCheckRoomEntity
+    suspend fun insertDeliveryBasic(deliveryBasicRoomEntity: DeliveryBasicRoomEntity)
 
-    suspend fun fetchDeliveryCompany(): DeliveryCompanyRoomEntity
+    suspend fun fetchDeliveryBasic(): List<DeliveryBasicEntity>
+
+    suspend fun fetchDeliveryCheck(deliveryCheckRequest: DeliveryCheckRequest): DeliveryCheckEntity
+
+    suspend fun fetchDeliveryCompany(): List<DeliveryCompanyEntity>
 }
