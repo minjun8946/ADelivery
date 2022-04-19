89f51748a0875cb3934509d82605f72d09032ee3
minjun8946 <minjun8946@gmail.com>
2022-04-19 22:53:09 +0900

    ⚡️:: cacheType

---
+++ domain/src/main/java/com/adelivery/domain/base/CacheType.kt
@@ -0,0 +1,12 @@
+package com.adelivery.domain.base
+
+data class CacheType(
+    val CacheType: Type? = Type.ALWAYS
+) {
+    enum class Type {
+        ALWAYS,
+        NEVER,
+        COMPARE,
+        ONES
+    }
+}
