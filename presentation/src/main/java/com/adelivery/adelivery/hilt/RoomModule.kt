a99a175ebb7659e1f0a363abadff22da56fe5a8d
minjun8946 <minjun8946@gmail.com>
2022-04-20 10:37:07 +0900

    ⚡️:: room module

---
+++ presentation/src/main/java/com/adelivery/adelivery/hilt/RoomModule.kt
@@ -0,0 +1,43 @@
+package com.adelivery.adelivery.hilt
+
+import android.content.Context
+import androidx.room.Room
+import com.adelivery.data.local.converter.DeliveryProgressConverter
+import com.adelivery.data.local.dao.DeliveryDao
+import com.adelivery.data.local.database.DeliveryDataBase
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    fun provideWalkHubDatabase(
+        @ApplicationContext context: Context,
+        moshi: Moshi
+    ): DeliveryDataBase = Room
+        .databaseBuilder(context, DeliveryDataBase::class.java, "WalkHubDataBase")
+        .addTypeConverter(DeliveryProgressConverter(moshi))
+        .build()
+
+    @Provides
+    fun provideChallengeDao(
+        walkHubDataBase: DeliveryDataBase
+    ): DeliveryDao = walkHubDataBase.deliveryDao()
+
+}
