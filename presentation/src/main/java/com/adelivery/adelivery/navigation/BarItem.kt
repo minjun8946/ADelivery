23cd432d1d096728dab5efdc4c6ec46576e6b147
minjun8946 <minjun8946@gmail.com>
2022-04-21 23:25:25 +0900

    ⚡️:: bar item

---
+++ presentation/src/main/java/com/adelivery/adelivery/navigation/BarItem.kt
@@ -0,0 +1,9 @@
+package com.adelivery.adelivery.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BarItem(
+    val title: String,
+    val image: ImageVector,
+    val route: String
+)
