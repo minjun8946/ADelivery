358bfbe93b9955bafa1e1d77ec1dbfdc67dee477
minjun8946 <minjun8946@gmail.com>
2022-04-21 23:25:38 +0900

    ⚡️:: bottom navigation bar

---
+++ presentation/src/main/java/com/adelivery/adelivery/navigation/BottomNavigationBar.kt
@@ -0,0 +1,38 @@
+package com.adelivery.adelivery.navigation
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.adelivery.adelivery.main.NavBarItems
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+
+    BottomNavigation {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry?.destination?.route
+
+        NavBarItems.BarItems.forEach { navItem ->
+            BottomNavigationItem(
+                selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = { Icon(imageVector = navItem.image, contentDescription = navItem.title) },
+                label = { Text(text = navItem.title) }
+            )
+        }
+    }
+}
