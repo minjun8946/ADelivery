3b256bf6749106f539177c1de3296c5f5fbb9495
minjun8946 <minjun8946@gmail.com>
2022-04-23 23:51:30 +0900

    🐛:: type converter class

--- data/src/main/java/com/adelivery/data/local/converter/TypeConverter.kt
+++ data/src/main/java/com/adelivery/data/local/converter/TypeConverter.kt
@@ -2,11 +2,7 @@ package com.adelivery.data.local.converter
 
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
-import com.adelivery.data.local.entity.DeliveryBasicRoomEntity
 import com.adelivery.data.local.entity.DeliveryCheckRoomEntity
-import com.adelivery.data.local.entity.DeliveryCompanyRoomEntity
-import com.adelivery.data.local.entity.Progresses
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
@@ -16,23 +12,23 @@ class DeliveryProgressConverter(
     private val moshi: Moshi
 ) {
     @TypeConverter
-    fun fromString(value: String): List<Progresses>? {
+    fun fromString(value: String): List<DeliveryCheckRoomEntity.CheckProgresses>? {
         val listType = Types.newParameterizedType(
             List::class.java,
-            Progresses::class.java
+            DeliveryCheckRoomEntity.CheckProgresses::class.java
         )
-        val adapter: JsonAdapter<List<Progresses>> =
+        val adapter: JsonAdapter<List<DeliveryCheckRoomEntity.CheckProgresses>> =
             moshi.adapter(listType)
         return adapter.fromJson(value)
     }
 
     @TypeConverter
-    fun fromProgress(value: List<Progresses>): String {
+    fun fromProgress(value: List<DeliveryCheckRoomEntity.CheckProgresses>): String {
         val listType = Types.newParameterizedType(
             List::class.java,
-            Progresses::class.java
+            DeliveryCheckRoomEntity.CheckProgresses::class.java
         )
-        val adapter: JsonAdapter<List<Progresses>> = moshi.adapter(listType)
+        val adapter: JsonAdapter<List<DeliveryCheckRoomEntity.CheckProgresses>> = moshi.adapter(listType)
         return adapter.toJson(value)
     }
 }
