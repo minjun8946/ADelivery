d9621cc3c13deda207e61e96ff15d67fa9518eab
minjun8946 <minjun8946@gmail.com>
2022-04-27 22:18:40 +0900

    ⚡️:: main activity

--- presentation/src/main/java/com/adelivery/adelivery/MainActivity.kt
+++ presentation/src/main/java/com/adelivery/adelivery/MainActivity.kt
@@ -3,10 +3,12 @@ package com.adelivery.adelivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Shop
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,17 +18,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.adelivery.adelivery.home.Home
+import com.adelivery.adelivery.home.HomeViewModel
 import com.adelivery.adelivery.mypage.MyPage
 import com.adelivery.adelivery.navigation.BottomNavigationBar
 import com.adelivery.adelivery.navigation.NavRoutes
 import com.adelivery.adelivery.ui.theme.ADeliveryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ADeliveryTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -44,9 +49,9 @@ fun MainScreen() {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("ADelivery") }, backgroundColor = Color.Transparent, navigationIcon = {
+            TopAppBar(title = { Text("ADelivery") }, backgroundColor = Color.White, navigationIcon = {
                 Icon(
-                    imageVector = Icons.Filled.List,
+                    imageVector = Icons.Filled.Shop,
                     contentDescription = "Main"
                 )
             })
