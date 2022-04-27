9a9b2540216420f0f387c1cee8850d31b3afb3b4
minjun8946 <minjun8946@gmail.com>
2022-04-27 22:17:27 +0900

    ⚡️:: fetch delivery list

--- domain/src/main/java/com/adelivery/domain/repository/DeliveryRepository.kt
+++ domain/src/main/java/com/adelivery/domain/repository/DeliveryRepository.kt
@@ -1,15 +1,15 @@
 package com.adelivery.domain.repository
 
-import com.adelivery.domain.entity.DeliveryBasicEntity
 import com.adelivery.domain.entity.DeliveryCheckEntity
 import com.adelivery.domain.entity.DeliveryCompanyEntity
 import com.adelivery.domain.param.DeliveryCheckParam
 import kotlinx.coroutines.flow.Flow
 
 interface DeliveryRepository {
-    suspend fun fetchDeliveryBasic(): Flow<List<DeliveryBasicEntity>>
 
     suspend fun fetchDeliveryCheck(deliveryCheckParam: DeliveryCheckParam): Flow<DeliveryCheckEntity>
+
+    suspend fun fetchDeliveryList(): Flow<List<DeliveryCheckEntity>>
 
     suspend fun fetchDeliveryCompany(): Flow<List<DeliveryCompanyEntity>>
 }
