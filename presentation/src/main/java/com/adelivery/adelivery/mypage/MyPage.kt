3047310bf00a85bb737038f88171d9e8110e1c3f
minjun8946 <minjun8946@gmail.com>
2022-04-27 22:20:55 +0900

    ⚡️:: my page

--- presentation/src/main/java/com/adelivery/adelivery/mypage/MyPage.kt
+++ presentation/src/main/java/com/adelivery/adelivery/mypage/MyPage.kt
@@ -1,27 +1,49 @@
 package com.adelivery.adelivery.mypage
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.adelivery.adelivery.component.DeliveryCheck
+import com.adelivery.adelivery.component.DeliveryProgressColumn
+import com.adelivery.domain.entity.DeliveryCheckEntity
 
 
 @Composable
-fun MyPage() {
+fun MyPage(viewModel: MyPageViewModel = hiltViewModel()) {
+
+    viewModel.setEvent(MyPageContract.Event.OnFetchDeliveryList)
+
+    val state = viewModel.uiState.collectAsState().value
     Box(modifier = Modifier.fillMaxSize()) {
-        Icon(
-            imageVector = Icons.Filled.ShoppingCart,
-            contentDescription = "myPage",
-            tint = Color.Blue,
-            modifier = Modifier.size(150.dp)
-                .align(Alignment.Center)
-        )
+        DeliveryList(state = state)
+    }
+}
+
+@Composable
+fun DeliveryList(state: MyPageContract.State){
+    when(state.deliveryList){
+        is MyPageContract.DeliveryListState.Success -> { DeliveryListColumn(data = state.deliveryList.deliveryList)}
+        else -> { println(state.deliveryList)}
+    }
+}
+
+@Composable
+fun DeliveryListColumn(data: List<DeliveryCheckEntity>){
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(12.dp)
+        .padding(bottom = 50.dp)) {
+        Box{
+            data.map { DeliveryCheck(data = it) }
+        }
     }
 }
