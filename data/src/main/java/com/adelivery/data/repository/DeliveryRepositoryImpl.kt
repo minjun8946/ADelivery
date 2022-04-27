9c5f30f01a020c282e77d3d64230ec376cf98fc2
minjun8946 <minjun8946@gmail.com>
2022-04-27 22:16:28 +0900

    ⚡️:: repository impl

--- data/src/main/java/com/adelivery/data/repository/DeliveryRepositoryImpl.kt
+++ data/src/main/java/com/adelivery/data/repository/DeliveryRepositoryImpl.kt
@@ -6,7 +6,6 @@ import com.adelivery.data.local.entity.*
 import com.adelivery.data.remote.datasource.RemoteDeliveryDataSource
 import com.adelivery.data.remote.request.toEntity
 import com.adelivery.domain.base.CacheType
-import com.adelivery.domain.entity.DeliveryBasicEntity
 import com.adelivery.domain.entity.DeliveryCheckEntity
 import com.adelivery.domain.entity.DeliveryCompanyEntity
 import com.adelivery.domain.param.DeliveryCheckParam
@@ -20,21 +19,22 @@ class DeliveryRepositoryImpl @Inject constructor(
     private val localDeliveryDataSource: LocalDeliveryDataSource
 ): DeliveryRepository {
 
-    override suspend fun fetchDeliveryBasic():Flow<List<DeliveryBasicEntity>> =
-        flow { localDeliveryDataSource.fetchDeliveryBasic() }
-
     override suspend fun fetchDeliveryCheck(deliveryCheckParam: DeliveryCheckParam): Flow<DeliveryCheckEntity> =
         CacheUtil<DeliveryCheckEntity>()
             .localDataSource { localDeliveryDataSource.fetchDeliveryCheck(deliveryCheckParam.toEntity()) }
-            .remoteDataSource { remoteDeliveryDataSource.patchDeliveryCheck(deliveryCheckParam.toEntity()) }
+            .remoteDataSource { remoteDeliveryDataSource.patchDeliveryCheck(deliveryCheckParam.toEntity())!! }
             .insertRoom { localDeliveryDataSource.insertDeliveryCheck(it.toCheckRoomEntity()) }
-            .insertRoom { localDeliveryDataSource.insertDeliveryBasic(it.toBasicRoomEntity()) }
-            .cacheType(CacheType(CacheType = CacheType.Type.ALWAYS))
+            .cacheType(CacheType(CacheType.Type.ALWAYS))
+
+    override suspend fun fetchDeliveryList(): Flow<List<DeliveryCheckEntity>> =
+        flow { emit(localDeliveryDataSource.fetchDeliveryList()) }
+
 
     override suspend fun fetchDeliveryCompany(): Flow<List<DeliveryCompanyEntity>> =
         CacheUtil<List<DeliveryCompanyEntity>>()
             .localDataSource { localDeliveryDataSource.fetchDeliveryCompany() }
             .remoteDataSource { remoteDeliveryDataSource.fetchDeliveryCompany() }
             .insertRoom { localDeliveryDataSource.insertDeliveryCompany(it.map { it.toEntity() }) }
-            .cacheType(CacheType((CacheType.Type.ONES)))
+            .cacheType(CacheType((CacheType.Type.ALWAYS)))
 }
+
